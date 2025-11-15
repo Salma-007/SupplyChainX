0c5a0b76cd9226a6b374d9d86bc9e3928c3e8439
Salma-007 <salmaelallali11@gmail.com>
2025-11-15 18:02:20 +0100

    modif de applicaton-test

--- src/test/java/com/example/supplychainx/test_approvisionnement/RawMaterialControllerIntegrationTest.java
+++ src/test/java/com/example/supplychainx/test_approvisionnement/RawMaterialControllerIntegrationTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,6 +34,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 public class RawMaterialControllerIntegrationTest {
 
     @Container
+    @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
     @Autowired
