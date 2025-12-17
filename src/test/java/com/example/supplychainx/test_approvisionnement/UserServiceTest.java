c4c868a424ae47b332716d499471348e85775021
Salma-007 <salmaelallali11@gmail.com>
2025-12-17 14:29:05 +0100

    fixing create user test error

--- src/test/java/com/example/supplychainx/test_approvisionnement/UserServiceTest.java
+++ src/test/java/com/example/supplychainx/test_approvisionnement/UserServiceTest.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +29,9 @@ import static org.mockito.Mockito.*;
     private UserRepository userRepository;
     @Mock
     private UserMapper userMapper;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserService userService;
